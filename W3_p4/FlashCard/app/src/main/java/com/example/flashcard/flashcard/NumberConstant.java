2152dc990485c1f9ebcc391525335e83ba0aa12c
qyzhang23 <qyzhang0305@gmail.com>
2022-02-13 18:53:38 -0500

    p4-

--- W3_p4/FlashCard/app/src/main/java/com/example/flashcard/flashcard/NumberConstant.java
+++
@@ -1,7 +0,0 @@
-package com.example.flashcard.flashcard;
-
-public class NumberConstant {
-    public static final int hardBound = 100;
-    public static final int mediumBound = 21;
-    public static final int totalQuestion = 10;
-}
--- W3_p4/README.md
+++
@@ -1,2 +0,0 @@
-A valid account username: guest  password: 123456  
-You can also click "sign in as guest" button to sign in derectly
